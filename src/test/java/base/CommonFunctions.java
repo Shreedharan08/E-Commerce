b3a7a9ed3d651fd364203d7bd05bf7e5238912a6
shree <shree@LAPTOP-6VGQKE2B>
2024-06-23 12:39:31 +0530

    Add Screenshot method and Added Search Feature file

--- src/test/java/base/CommonFunctions.java
+++ src/test/java/base/CommonFunctions.java
@@ -11,7 +11,10 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.LogManager;
 
+import org.apache.commons.io.FileUtils;
 import org.checkerframework.common.value.qual.StaticallyExecutable;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -22,23 +25,27 @@ import constants.Contants;
 import io.cucumber.core.options.Constants;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import page_Objects.HomePage_PageObjects;
 import page_Objects.Login_PageObjects;
 
 public class CommonFunctions {
 	public static WebDriver driver;
-	public static Properties pro ;
+	public static String name = null;
+	public static Properties pro;
 	private static org.apache.logging.log4j.Logger log = org.apache.logging.log4j.LogManager.getLogger();
-	
+
 	@Before
-	public void beforescenario() {
+	public void beforescenario(Scenario scenario) {
 		try {
+			name = scenario.getName();
 			log.info("Execution Started");
 			launchBroswer();
-			log.info(Contants.broswerinput+" "+"Browser Launched Successfully");
+			log.info(Contants.broswerinput + " " + "Browser Launched Successfully");
 			pagefactory();
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(e);
 		}
 	}
 
@@ -48,45 +55,57 @@ public class CommonFunctions {
 			switch (Contants.broswerinput) {
 			case "Chrome":
 				WebDriverManager.chromedriver().setup();
-				driver= new ChromeDriver();
+				driver = new ChromeDriver();
 				driver.manage().window().maximize();
-				driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+				driver.get(Contants.Url);
 				break;
 			case "Firefox":
 				WebDriverManager.firefoxdriver().setup();
-				driver= new FirefoxDriver();
+				driver = new FirefoxDriver();
 				driver.manage().window().maximize();
-				driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+				driver.get(Contants.Url);
 				break;
-			default :
+			default:
 				System.out.println("Please provide broswer");
 				break;
 			}
 		} catch (Exception e) {
-			
+			log.error(e);
 		}
-		
-	
-	
-}
-	public static Properties load() throws IOException {
-		    String fileName = "config.properties"; // Replace "config.properties" with your actual filename
-		    InputStream inputStream = CommonFunctions.class.getClassLoader().getResourceAsStream(fileName);
-		    if (inputStream == null) {
-		      throw new IOException("Failed to find config.properties file");
-		    }
-		    pro = new Properties();
-		    pro.load(inputStream);
-		    inputStream.close();
-		    return pro;		
+
 	}
-	
+
+	public static Properties load() throws IOException {
+		String fileName = "config.properties"; // Replace "config.properties" with your actual filename
+		InputStream inputStream = CommonFunctions.class.getClassLoader().getResourceAsStream(fileName);
+		if (inputStream == null) {
+			throw new IOException("Failed to find config.properties file");
+		}
+		pro = new Properties();
+		pro.load(inputStream);
+		inputStream.close();
+		return pro;
+	}
+
 	public static void pagefactory() {
 		PageFactory.initElements(driver, Login_PageObjects.logindetails());
+		PageFactory.initElements(driver, HomePage_PageObjects.homedetails());
 	}
-@After
-public void teardown() {
-	driver.close();
-	
-}
+
+	public static void takescreenshot() {
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(src, new File(name + ".png"));
+		} catch (Exception e) {
+			log.error(e);
+		}
+	}
+
+	@After
+	public void teardown() {
+		driver.close();
+
+	}
 }
